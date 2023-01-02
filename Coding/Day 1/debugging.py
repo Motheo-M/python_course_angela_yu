b91730f739ad4ef42bc97e8ef5cb6741ddc218a7
Motheo <motheotb@gmail.com>
2023-01-02 14:38:54 +0200

    first commit

---
+++ Coding/Day 1/debugging.py
@@ -0,0 +1,5 @@
+# Basic string debugging using python
+print("Day 1 - String Manipulation"
+      "\nString Concatenation is done with the \"+\" sign."
+      "\ne.g. print(\"Hello \" + \"world\")"
+      "\nNew lines can be created with a backslash and n.")
